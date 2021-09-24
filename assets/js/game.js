249e4867c1f6bceb439996601cca1c91f7a68ff0
mixedbeans <spencerbiehn09@gmail.com>
2021-09-23 21:32:58 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,6 +10,7 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
 // FIGHT FUNCTION
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -25,14 +26,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on players attack value
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0,enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -51,7 +54,9 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -80,7 +85,7 @@ var startGame = function() {
     if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
 
       // if player still alive and if we're not at last enemy in array
@@ -176,7 +181,12 @@ var shop = function() {
   }
 };
 
+// function to generate random numeric value
+var randomNumber = function(min,max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
 
+  return value;
+};
 
 
 startGame();
